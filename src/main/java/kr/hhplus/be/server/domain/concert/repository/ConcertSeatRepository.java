69d4c954539307cbfd87b3a79167022b681d5b70
ych <chae6127@gmail.com>
2025-04-17 00:52:37 +0900

    feat : 콘서트 좌석 상태 변경 어노테이션 추가

--- src/main/java/kr/hhplus/be/server/domain/concert/repository/ConcertSeatRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/concert/repository/ConcertSeatRepository.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.domain.concert.repository;
 
 import kr.hhplus.be.server.domain.concert.entity.ConcertSeat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,6 +18,7 @@ public interface ConcertSeatRepository extends JpaRepository<ConcertSeat, Long> 
     @Query("SELECT seat_id, concert_date_id, price, status, seat_number  FROM ConcertSeat WHERE seat_id = :seat_id")
     ConcertSeat getSeat(@Param("seat_id")long seat_id);
 
+    @Modifying
     @Query("UPDATE ConcertSeat Set status = :#{concertSeat.status} WHERE seat_id = :#{concertSeat.seat_id}")
     int updateSeat(ConcertSeat concertSeat);
 }
